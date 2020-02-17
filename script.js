8fdb800d12fecbcf2c2217853240b0bab42ef734
Chris Soltren <csoltren1@yahoo.com>
2020-02-17 16:57:05 -0500

    Got local storage to work

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+var a = [];
+if (localStorage.getItem("cities")) {
+    a = JSON.parse(localStorage.getItem("cities"))
+};
+for (var i = 0; i < a.length; i++) {
+    $(".cityHistory").append(`
+        <button class="cityButton"> ${a[i]} </button>
+    `)
+}
+
 function getweatherData() {
     var key = "67b9879b2c60fa4a3b20f137287b873a";
-    var city = "New%20York";
+    var city = a[0];
     var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key;
 
 
@@ -16,14 +26,19 @@ function getweatherData() {
             }
         }
 
-        $("citiesDisplay").append(JSON.stringify)
+        $("cityDisplay").append(response.city.name)
         $("#tempDisplay").append(JSON.stringify(parseInt((response.list[0].main.temp) - 273.15) * 9 / 5 + 32 + String.fromCharCode(176) + 'F'))
         $("#humDisplay").append(JSON.stringify(response.list[0].main.humidity + "%"))
         $("#windDisplay").append(JSON.stringify(response.list[0].wind.speed))
-        $(".card-content").append(JSON.stringify(parseInt((response.list[i].main.temp) - 273.15) * 9 / 5 + 32 + String.fromCharCode(176) + 'F'))
-
 
     })
 }
 
-getweatherData() 
+$("#search").click(function () {
+
+    var city = $(this).prev().val();
+    a.push(city);
+    localStorage.setItem("cities", JSON.stringify(a));
+    getweatherData();
+})
+// getweatherData()
