552b665a102ef33bebf679a450e079d8b62f9191
Chris Soltren <csoltren1@yahoo.com>
2020-02-17 20:16:04 -0500

    Got my cards yeaaaa

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ for (var i = 0; i < a.length; i++) {
     `)
 }
 
+var dayLength = 5;
+$(".card-deck").empty();
+
+
+for (var i = 0; i < dayLength; i++) {
+    var dayForecast = $("<div>").addClass("card");
+    var forecastBody = $("<div>").addClass("card-body");
+    var cardTitle = $("<h5>").addClass("card-title").text(moment().add(i + 1, 'days').format('l'));
+    forecastBody.append(cardTitle);
+
+
+
+    dayForecast.append(forecastBody);
+    $(".card-deck").append(dayForecast);
+
+}
+
 function getweatherData() {
     var key = "67b9879b2c60fa4a3b20f137287b873a";
     var city = a[0];
@@ -26,10 +43,10 @@ function getweatherData() {
             }
         }
 
-        $("cityDisplay").append(response.city.name)
-        $("#tempDisplay").append(JSON.stringify(parseInt((response.list[0].main.temp) - 273.15) * 9 / 5 + 32 + String.fromCharCode(176) + 'F'))
-        $("#humDisplay").append(JSON.stringify(response.list[0].main.humidity + "%"))
-        $("#windDisplay").append(JSON.stringify(response.list[0].wind.speed))
+        $("cityDisplay").text(response.city.name)
+        $("#tempDisplay").text(JSON.stringify(parseInt((response.list[0].main.temp) - 273.15) * 9 / 5 + 32 + String.fromCharCode(176) + 'F'))
+        $("#humDisplay").text(JSON.stringify(response.list[0].main.humidity + "%"))
+        $("#windDisplay").text(JSON.stringify(response.list[0].wind.speed + "MPH"))
 
     })
 }
@@ -37,8 +54,11 @@ function getweatherData() {
 $("#search").click(function () {
 
     var city = $(this).prev().val();
-    a.push(city);
+    if (($.inArray(city, a) === -1 && city !== "")) {
+        a.unshift(city);
+    }
     localStorage.setItem("cities", JSON.stringify(a));
     getweatherData();
 })
+
 // getweatherData()
