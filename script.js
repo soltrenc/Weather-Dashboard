843f966324477acbd4cc7593975a2bf32add34d9
Chris Soltren <csoltren1@yahoo.com>
2020-02-18 00:25:32 -0500

    Everything works, just need better styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var a = [];
+var city;
 if (localStorage.getItem("cities")) {
     a = JSON.parse(localStorage.getItem("cities"))
 };
@@ -8,27 +9,10 @@ for (var i = 0; i < a.length; i++) {
     `)
 }
 
-var dayLength = 5;
-$(".card-deck").empty();
-
-
-for (var i = 0; i < dayLength; i++) {
-    var dayForecast = $("<div>").addClass("card");
-    var forecastBody = $("<div>").addClass("card-body");
-    var cardTitle = $("<h5>").addClass("card-title").text(moment().add(i + 1, 'days').format('l'));
-    forecastBody.append(cardTitle);
-
-
-
-    dayForecast.append(forecastBody);
-    $(".card-deck").append(dayForecast);
-
-}
-
 function getweatherData() {
     var key = "67b9879b2c60fa4a3b20f137287b873a";
-    var city = a[0];
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key;
+    //var city = a[0];
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&appid=" + key;
 
 
     $.ajax({
@@ -44,16 +28,39 @@ function getweatherData() {
         }
 
         $("cityDisplay").text(response.city.name)
-        $("#tempDisplay").text(JSON.stringify(parseInt((response.list[0].main.temp) - 273.15) * 9 / 5 + 32 + String.fromCharCode(176) + 'F'))
+        $("#tempDisplay").text(response.list[0].main.temp + "F")
         $("#humDisplay").text(JSON.stringify(response.list[0].main.humidity + "%"))
         $("#windDisplay").text(JSON.stringify(response.list[0].wind.speed + "MPH"))
+        var dayLength = 5;
+        $(".card-deck").empty();
 
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + key + "&lat={lat}&lon={lon}",
+            method: "GET"
+        }).then(function (data) {
+            console.log(data);
+        })
+
+        for (var i = 0; i < dayLength; i++) {
+            var dayForecast = $("<div>").addClass("card");
+            var forecastBody = $("<div>").addClass("card-body");
+            var cardTitle = $("<h5>").addClass("card-title").text(moment().add(i + 1, 'days').format('l'));
+            var dayTemp = $("<p>").text("Temperature:" + (response.list[i + 1].main.temp + "F"));
+            var dayHumid = $("<p>").text("Humidity:" + (JSON.stringify(response.list[i + 1].main.humidity) + "%"));
+            forecastBody.append(cardTitle);
+            forecastBody.append(dayTemp);
+            forecastBody.append(dayHumid);
+
+            dayForecast.append(forecastBody);
+            $(".card-deck").append(dayForecast);
+
+        }
     })
 }
 
 $("#search").click(function () {
 
-    var city = $(this).prev().val();
+    city = $(this).prev().val();
     if (($.inArray(city, a) === -1 && city !== "")) {
         a.unshift(city);
     }
@@ -61,4 +68,8 @@ $("#search").click(function () {
     getweatherData();
 })
 
-// getweatherData()
+$(".cityButton").click(function () {
+    city = $(this).text();
+    console.log("The city being clicked is " + city);
+    getweatherData()
+});
